75ad69acff09619ceff057f141160af0754883f3
muhi111 <107345594+muhi111@users.noreply.github.com>
2024-08-15 22:44:34 +0900

    feat: Update database driver to MySQL
    The code changes in this commit update the database driver from PostgreSQL to MySQL. This change is necessary to connect to the MySQL database specified in the docker-compose.yml file. The new driver is imported from `gorm.io/driver/mysql` and the connection URL is modified accordingly. This commit also updates the docker-compose.yml file to use the `mariadb:10.5` image and sets the necessary environment variables for MySQL. The healthcheck command is also modified to use a custom script for checking the database's health.

--- backend/db/db.go
+++ backend/db/db.go
@@ -6,7 +6,7 @@ import (
 	"os"
 
 	"github.com/joho/godotenv"
-	"gorm.io/driver/postgres"
+	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
@@ -15,13 +15,13 @@ func NewDB() *gorm.DB {
 	if err != nil {
 		log.Fatalln(err)
 	}
-	url := fmt.Sprintf("postgres://%s:%s@%s:%s/%s",
+	url := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASSWORD"),
 		os.Getenv("DB_HOST"),
 		os.Getenv("DB_PORT"),
 		os.Getenv("DB_NAME"))
-	db, err := gorm.Open(postgres.Open(url), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(url), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("🔴 Error connecting to database: %s", err)
 	}
