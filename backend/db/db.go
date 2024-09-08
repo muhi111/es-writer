22c93f7b03a7aa2733086320da649f927352690f
muhi111 <107345594+muhi111@users.noreply.github.com>
2024-09-08 15:25:44 +0900

    fix: Refactor migration code to remove dependency on godotenv package

--- backend/db/db.go
+++ backend/db/db.go
@@ -5,16 +5,11 @@ import (
 	"log"
 	"os"
 
-	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func NewDB() *gorm.DB {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatalln(err)
-	}
 	url := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASSWORD"),
